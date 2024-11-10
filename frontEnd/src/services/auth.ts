55c827606ba67f16263382aab1946f15c0bb4dc8
Stan-breaks <stanleymwendwa03@gmail.com>
2024-11-10 12:57:25 +0300

    doctor type

--- frontEnd/src/services/auth.ts
+++ frontEnd/src/services/auth.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { LoginCredentials, RegisterData, AuthResponse } from "../types/auth";
+import {
+  LoginCredentials,
+  RegisterData,
+  AuthResponse,
+  DoctorRegisterData,
+} from "../types/auth";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -11,6 +16,10 @@ export const authService = {
 
   async register(data: RegisterData): Promise<AuthResponse> {
     const response = await axios.post(`${API_URL}/auth/register`, data);
+    return response.data;
+  },
+  async registerDoctor(data: DoctorRegisterData): Promise<AuthResponse> {
+    const response = await axios.post(`${API_URL}/auth/registerDoctor`, data);
     return response.data;
   },
 };
